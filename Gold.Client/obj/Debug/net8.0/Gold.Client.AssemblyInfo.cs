c462a822a7cb96bfa54d54fa9522a1bd0c534adb
Fabrice Rosito <fabrice.rosito@gmail.com>
2024-02-24 22:25:21 +0100

    feat(core): Add fetch

--- Gold.Client/obj/Debug/net8.0/Gold.Client.AssemblyInfo.cs
+++ Gold.Client/obj/Debug/net8.0/Gold.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gold.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01283fe76de979059dbb15d75b543964903289ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f28e5d00a29c50953ada688669a3449a1842a2fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gold.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gold.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Gold/obj/Debug/net8.0/Gold.AssemblyInfo.cs
+++ Gold/obj/Debug/net8.0/Gold.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gold")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01283fe76de979059dbb15d75b543964903289ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f28e5d00a29c50953ada688669a3449a1842a2fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gold")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gold")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
